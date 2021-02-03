7d67b8e23190ee059cc8f520ba3c22d0f58e8a45
Niklas Marion <niklas.marion@gmx.de>
2021-02-03 13:05:12 +0000

    chore: add ackee + meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,4 +7,37 @@ export default theme({
     content: {
         liveEdit: true
     },
+    buildModules: [
+        'nuxt-ackee'
+    ],
+    ackee: {
+        server: 'https://ackee-niklas.netlify.app',
+        domainId: '81458316-3dbb-436c-a19c-f51b60b262cf',
+        detailed: true
+    },
+    head: {
+        htmlAttrs: {
+            lang: 'de'
+        },
+        title: 'Betriebssystemeinführung',
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            {
+                hid: 'og:title',
+                name: 'og:title',
+                content: 'Betriebssystemeinführung'
+            },
+            {
+                hid: 'og:url',
+                name: 'og:url',
+                content: 'https://htw-bse.netlify.app'
+            },
+            {
+                hid: 'og:type',
+                name: 'og:type',
+                content: 'website'
+            }
+        ]
+    },
 });
